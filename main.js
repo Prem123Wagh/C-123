6b3844ed46a325caa0f9a133476f339e02653a8e
Prem123Wagh <154726145+Prem123Wagh@users.noreply.github.com>
2023-12-28 08:45:38 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560,150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose',gotPoses)
+}
+function modelLoaded(){
+    console.log('PoseNet Is Initialized!');
+}
+function draw() {
+    background('#969A97');
+
+    document.getElementById("square_side").innerHtml = "Width And Height of a Square will be = " + difference +"px"
+    fill('#F90093');
+    stroke('#F90093');
+    square(noseX, noseY, 100);
+}
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = resualt[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX =" + noseX +"noseY=" + noseY);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.lof("leftWristX = " + leftWristX + "rightWristX ="+ rightWristX + "difference = " + difference);
+    }
+}
+
+noseX=0;
+noseY=0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
